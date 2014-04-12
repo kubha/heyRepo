09708263e0795321f0a97472cfd51392425145c6
Firmino <Firmino@cmer>
2014-04-12 22:21:16 +0200

    Commit the entire project

---
+++ initReProject/src/initReProject/Maboto.java
@@ -0,0 +1,10 @@
+package initReProject;
+
+public class Maboto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github Modified");
+	}
+
+}
