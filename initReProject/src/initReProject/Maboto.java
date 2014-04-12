06ce0e8e01d46ee2394cd96a0bd86a28187aafe7
Firmino <Firmino@cmer>
2014-04-12 22:29:05 +0200

    3 Commint

--- initReProject/src/initReProject/Maboto.java
+++ initReProject/src/initReProject/Maboto.java
@@ -4,7 +4,8 @@ public class Maboto {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Github Modified");
+		System.out.println("Third Hello Github Modified");
+		NewClass.speak();
 	}
 
 }
--- initReProject/src/initReProject/NewClass.java
+++ initReProject/src/initReProject/NewClass.java
@@ -2,4 +2,8 @@ package initReProject;
 
 public class NewClass {
 
+	public static void speak()
+	{
+		System.out.println("I'm the class NewClass");
+	}
 }
